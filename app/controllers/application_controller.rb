0774fd85613c5a3443218be39f5d862e1d64f8d4
Bill Tihen <btihen@gmail.com>
2022-01-30 19:53:58 +0100

    routing fixed, landing works, users inaccessible

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :users_only
+
+  def current_user(user_id = session[:user_id])
+    @current_user ||= User.find_by(id: user_id)
+  end
+
+  private
+  # send unauthorized users to the root page
+  def users_only
+    return if current_user.present?
+
+    redirect_back(fallback_location: root_path)
+  end
 end
--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,6 @@
 class LandingController < ApplicationController
+  skip_before_action :users_only, only: :index
+
   def index
   end
 end
